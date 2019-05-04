cc5363177c4d81c71b60219fe2aae656bc25a02f
Juan David Bolaños <juan-bol98@hotmail.com>
2019-05-04 11:52:02 -0500

    Revert "Se incluye reporte en consola"
    This reverts commit 8c2e0a254d89af8b630f3868d14b47138f88ca21.

--- protractor/helpers/reporter.ts
+++
@@ -1,9 +0,0 @@
-import { SpecReporter } from 'jasmine-spec-reporter';
-
-export let reporter = () => {
-  jasmine.getEnv().addReporter(new SpecReporter({
-    spec: {
-      displayStacktrace: true
-    }
-  }));
-};
